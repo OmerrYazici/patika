f75e9783b387c1be01d22b581ab6702cbb45a50f
OmerrYazici <88666841+OmerrYazici@users.noreply.github.com>
2022-08-10 15:21:42 +0300

    örnek

---
+++ Mantıksal_hatalar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class program{
+    static void Main(string[] args){
+        try{
+            System.Console.WriteLine("Bir sayi giriniz.");
+            int sayi=Convert.ToInt32 (Console.ReadLine());
+            System.Console.WriteLine("girmiş oldugunuz sayi:"+sayi);
+        }
+        catch(Exception ex){
+            System.Console.WriteLine("Hata: "+ex.Message.ToString());
+        }
+        finally{
+            System.Console.WriteLine("islemi tamamladınız.");
+        }
+    }
+}
